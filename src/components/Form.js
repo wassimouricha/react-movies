0f215190a7357589d27a6539e56700cd2cfc8ac2
wassim <wbouricha5@gmail.com>
2022-02-03 14:11:02 +0100

    v2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Form = () => {
+    const [moviesData, setMoviesData] = useState([]);
 
-
+useEffect(() => {
+    axios.get(
+        ` https://api.themoviedb.org/3/search/movie?api_key=278c4d8a90056d4bdf066f2e7609e2b7&query=star&language=fr-FR`
+    ).then((res) => setMoviesData(res.data.results));
+}, []);
 
 
 
@@ -14,6 +20,14 @@ const Form = () => {
                  <input type="text" placeholder="Entrez le titre d'un film"  id='search-input'/>
                  <input type="submit"  value="Rechercher"/>
              </form>
+
+             <div className="btn-sort-container">
+                 <div className="btn-sort" id="goodToBad">Top <span>&#8640;</span></div>
+                 <div className="btn-sort" id="badToGood">FLop <span>&#8640;</span></div>
+             </div>
+         </div>
+         <div className="result">
+             {moviesData.map((movie)=> <h2>{movie.title}</h2>)}
          </div>
      </div>
     );
