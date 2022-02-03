c87462b5b3cb3bab12de699504ec5c5d30dc99ba
wassim <wbouricha5@gmail.com>
2022-02-03 15:54:19 +0100

    v3

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,77 @@ import React from 'react';
 const Card = ({ movie }) => {
     const dateFormater = (date) => {
         let [yy, mm, dd] = date.split("-");
-        return [dd, mm, yy].join("/") ;  
+        return [dd, mm, yy].join("/");  
         // enn mettant join puis le / ma fonction va comprendre que dans mon array entre les caractères il va placer un /
+    };
+
+    const genreFinder = () => {
+        let genreArray = [];
+        for (let i = 0; i < movie.genre_ids.length; i++){
+            switch (movie.genre_ids[i]) {
+                case 28:
+          genreArray.push(`Action`);
+          break;
+        case 12:
+          genreArray.push(`Aventure`);
+          break;
+        case 16:
+          genreArray.push(`Animation`);
+          break;
+        case 35:
+          genreArray.push(`Comédie`);
+          break;
+        case 80:
+          genreArray.push(`Policier`);
+          break;
+        case 99:
+          genreArray.push(`Documentaire`);
+          break;
+        case 18:
+          genreArray.push(`Drame`);
+          break;
+        case 10751:
+          genreArray.push(`Famille`);
+          break;
+        case 14:
+          genreArray.push(`Fantasy`);
+          break;
+        case 36:
+          genreArray.push(`Histoire`);
+          break;
+        case 27:
+          genreArray.push(`Horreur`);
+          break;
+        case 10402:
+          genreArray.push(`Musique`);
+          break;
+        case 9648:
+          genreArray.push(`Mystère`);
+          break;
+        case 10749:
+          genreArray.push(`Romance`);
+          break;
+        case 878:
+          genreArray.push(`Science-fiction`);
+          break;
+        case 10770:
+          genreArray.push(`Téléfilm`);
+          break;
+        case 53:
+          genreArray.push(`Thriller`);
+          break;
+        case 10752:
+          genreArray.push(`Guerre`);
+          break;
+        case 37:
+          genreArray.push(`Western`);
+          break;
+        default:
+          break;
+            }       
+        }
+        
+        return genreArray.map((genre) => <li key={genre}>{genre}</li>);
     };
 
 
@@ -16,6 +85,10 @@ const Card = ({ movie }) => {
       <h2> {movie.title} </h2>
       {movie.release_date ? ( <h5>Sorti le : {dateFormater(movie.release_date)}</h5> ) : ""}
       <h4>{movie.vote_average}/10 <span> &#128293;</span> </h4>
+
+        <ul>{movie.genre_ids ? genreFinder() : movie.genres.map((genre, index) => (
+            <li key = {index}>{genre.name}</li>
+        ))}</ul> 
   </div>
     );
 };
